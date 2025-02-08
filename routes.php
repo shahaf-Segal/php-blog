e4a23c43aa3599996ae08256ae2af041b66ede13
shahaf-Segal <shahaf100@outlook.co.il>
2025-02-08 20:54:58 +0200

    added routes

---
+++ routes.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @var Core\Router $router
+ */
+$router->add('/', 'GET', 'HomeController@index');
+$router->add('/posts', 'GET', 'PostController@index');
+$router->add('/posts/{id}', 'GET', 'PostController@show');

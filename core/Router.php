835dae931eb11f296c3281ff45da46aaef92e362
shahaf-Segal <shahaf100@outlook.co.il>
2025-02-08 04:24:06 +0200

    started working on route finding

--- core/Router.php
+++ core/Router.php
@@ -4,8 +4,47 @@ namespace Core;
 
 class Router
 {
-    public function __construct()
+    protected array $routes = [];
+    public function add(string $uri, string $method, string $controller): void {}
+    public function dispatch(string $uri, string $method): string
     {
-        echo "Router";
+        return '';
+    }
+
+    protected function findRoute(string $uri, string $method): ?array
+    {
+        foreach ($this->routes as $route) {
+            if ($route['method'] !== $method) {
+                $params = $this->matchRoute($route['uri'], $uri);
+                if ($$params !== null) {
+                    return [...$route, 'params' => $params];
+                }
+            }
+        }
+    }
+
+    protected function matchRoute(string $routeUri, string $reqUri): ?array
+    {
+        $routeSegments = explode('/', trim($routeUri, '/'));
+        $reqSegments = explode('/', trim($reqUri, '/'));
+
+        if (count($routeSegments) !== count($reqSegments)) {
+            return null;
+        }
+        $params = [];
+        foreach ($routeSegments as $index => $segment) {
+            if (str_starts_with($segment, '{') && str_ends_with($segment, '}')) {
+                $params[trim($segment, '{}')] = $reqSegments[$index];
+            } elseif ($segment !== $reqSegments[$index]) {
+                return null;
+            }
+        }
+        return $params;
+    }
+
+    protected function callAction(string $controller, string $action, array $params): string
+    {
+        $controllerClass = "App\\Controllers\\$controller";
+        return (new $controllerClass)->$action($params);
     }
 }

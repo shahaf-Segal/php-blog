88d27b5fe475ed8603db55115c75432a24a562f3
shahaf-Segal <shahaf100@outlook.co.il>
2025-02-10 17:39:16 +0200

    added app

---
+++ core/App.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Core;
+
+class App
+{
+    protected static $container = [];
+    public static function bind(string $key, mixed $value): void
+    {
+        static::$container[$key] = $value;
+    }
+    public static function get(string $key): mixed
+    {
+        if (!array_key_exists($key, static::$container)) {
+            throw new \Exception("Key: $key not found in container");
+        }
+        return static::$container[$key];
+    }
+}

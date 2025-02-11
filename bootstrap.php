cb89b51fe084851be89ec02f52d3b1f8efad9fb7
shahaf-Segal <shahaf100@outlook.co.il>
2025-02-11 15:22:41 +0200

    added database config

--- bootstrap.php
+++ bootstrap.php
@@ -2,9 +2,10 @@
 
 declare(strict_types=1);
 require_once __DIR__ . '/vendor/autoload.php';
-
-$config = require __DIR__ . '/config.php';
+$config = require_once __DIR__ . '/config.php';
 
 use Core\App;
+use Core\DataBase;
 
 App::bind('config', $config);
+App::bind('database', new DataBase($config['database']));

715461cb105ab3ca8d2242cc2f4df78ca9eb1db1
shahaf-Segal <shahaf100@outlook.co.il>
2025-02-11 15:43:49 +0200

    added parts loading

--- bin/schema_load.php
+++ bin/schema_load.php
@@ -4,17 +4,19 @@ use Core\App;
 
 require_once __DIR__ . '/../bootstrap.php';
 
-echo "Loading schema...";
+
 
 $database = App::get('database');
 
 $schemafile = __DIR__ . '/../database/schema.sql';
 $sql = file_get_contents($schemafile);
-
+echo "Loading schema...\n";
 try {
-    $parts = explode(';', $sql);
-    var_dump($parts);
-    die;
+    $parts = array_filter(explode(';', $sql));
+    foreach ($parts as $part) {
+        $database->query($part);
+    }
+    echo "db schema loaded\n";
 } catch (PDOException $e) {
     throw new Exception("Unable to connect to DB");
 }

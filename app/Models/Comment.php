717855a62e71af4b86ab76f176ff53a19abe3c6a
shahaf-Segal <shahaf100@outlook.co.il>
2025-02-13 01:18:28 +0200

    base models

---
+++ app/Models/Comment.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Core\Model;
+
+class Comment extends Model
+{
+    protected static $table = 'comments';
+}
---
+++ app/Models/Post.php
@@ -0,0 +1,8 @@
+<?php
+
+use Core\Model;
+
+class Post extends Model
+{
+    protected static $table = 'posts';
+}

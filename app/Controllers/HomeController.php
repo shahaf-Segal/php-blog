a225be7c1a6c9dd31636b38d20483a7889bf0080
shahaf-Segal <shahaf100@outlook.co.il>
2025-02-13 01:19:06 +0200

    demo

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3,11 +3,19 @@
 namespace App\Controllers;
 
 use Core\View;
+use App\Models\User;
 
 class HomeController
 {
     public function index(): string
     {
+        $user = User::create([
+            'name' => 'John Doe',
+            'email' => '0kXnI@example.com',
+            'password' => 'password',
+            'salt' => 'salt'
+        ]);
+
         return View::render('Home/index', layout: 'layouts/main');
     }
 }

0a2cb76f6920583103566442833842710c4d63a3
shahaf-Segal <shahaf100@outlook.co.il>
2025-02-16 16:54:34 +0200

    added error handling works

--- bootstrap.php
+++ bootstrap.php
@@ -6,6 +6,10 @@ $config = require_once __DIR__ . '/config.php';
 
 use Core\App;
 use Core\DataBase;
+use Core\ErrorHandler;
+
+set_exception_handler([ErrorHandler::class, 'handleException']);
+set_error_handler([ErrorHandler::class, 'handleError']);
 
 App::bind('config', $config);
 App::bind('database', new DataBase($config['database']));

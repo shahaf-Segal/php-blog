665d3600f5842bb2293d869a56e641223381825c
shahaf-Segal <shahaf100@outlook.co.il>
2025-02-21 00:04:28 +0200

    posts: get recent implemented

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Core\Model;
+use Core\App;
 
 class Post extends Model
 {
     protected static $table = 'posts';
+
+    public static function getRecent(int $limit): array
+    {
+        $db = App::get('database');
+        return $db->fetchAll("SELECT * FROM" . static::$table . " ORDER BY created_at DESC LIMIT ?", [$limit]);
+    }
 }

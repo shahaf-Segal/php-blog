78ff7ba8858511b6c31a46d5564207bc97e1571c
shahaf-Segal <shahaf100@outlook.co.il>
2025-02-21 00:48:55 +0200

    added post class properties

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,6 +9,13 @@ class Post extends Model
 {
     protected static $table = 'posts';
 
+    public $id;
+    public $user_id;
+    public $title;
+    public $content;
+    public $views;
+    public $created_at;
+
     public static function getRecent(int $limit): array
     {
         $db = App::get('database');

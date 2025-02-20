b56eb0c388ac9db36333eb30c2f64422fb649f07
shahaf-Segal <shahaf100@outlook.co.il>
2025-02-21 00:27:02 +0200

    started getting posts in home page

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3,13 +3,13 @@
 namespace App\Controllers;
 
 use Core\View;
-use App\Models\User;
+use App\Models\Post;
 
 class HomeController
 {
     public function index(): string
     {
-
+        $posts = Post::getRecent(5);
         return View::render('Home/index', layout: 'layouts/main');
     }
 }

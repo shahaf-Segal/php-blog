0d8aefaf9efe142747f93d4462db7bea9b06d997
shahaf-Segal <shahaf100@outlook.co.il>
2025-02-21 00:34:55 +0200

    added class to model and removed create from array for a Per model structuring

--- core/Model.php
+++ core/Model.php
@@ -9,14 +9,20 @@ abstract class Model
     public static function all(): array
     {
         $db = App::get('database');
-        $results = $db->fetchAll("SELECT * FROM " . static::$table);
-        return array_map([static::class, 'createFromArray'], $results);
+        return $db->fetchAll(
+            "SELECT * FROM " . static::$table,
+            [],
+            static::class
+        );
     }
     public static function find(mixed $id): static|null
     {
         $db = App::get('database');
-        $result = $db->fetch("SELECT * FROM " . static::$table . " WHERE id = :id", ['id' => $id]);
-        return $result ? static::createFromArray($result) : null;
+        return $db->fetch(
+            "SELECT * FROM " . static::$table . " WHERE id = ?",
+            [$id],
+            static::class
+        );
     }
     public static function create(array $data): static
     {
@@ -26,16 +32,5 @@ abstract class Model
         $sql = "INSERT INTO " . static::$table . " ($collumns) VALUES ($placeholders)";
         $db->query($sql, $data);
         return static::find($db->lastInsertId());
-    }
-
-    protected static function createFromArray(array $data): static
-    {
-        $model = new static();
-        foreach ($data as $key => $value) {
-            if (property_exists($model, $key)) {
-                $model->{$key} = $value;
-            }
-        }
-        return $model;
     }
 }

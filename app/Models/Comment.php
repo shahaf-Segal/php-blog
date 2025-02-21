412984b26133ffd49b4f306c7b5d4b36e77f4431
shahaf-Segal <shahaf100@outlook.co.il>
2025-02-21 14:49:00 +0200

    adding model for comments

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Core\App;
 use Core\Model;
 
 class Comment extends Model
 {
     protected static $table = 'comments';
+    public $id;
+    public $content;
+    public $post_id;
+    public $user_id;
+    public $created_at;
+
+    public static function getForPost(int $postId): array
+    {
+        $db = App::get('database');
+        return $db->fetchAll(
+            "SELECT * FROM " . static::$table . " WHERE post_id = ? ORDER BY created_at DESC",
+            [$postId],
+            static::class
+        );
+    }
 }

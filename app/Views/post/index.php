584f00948f0652dfbbebf85f4a0892d50cd48a3c
shahaf-Segal <shahaf100@outlook.co.il>
2025-02-22 22:37:26 +0200

    added search in posts

--- app/Views/post/index.php
+++ app/Views/post/index.php
@@ -1,6 +1,12 @@
 <div>
     <h1>Posts</h1>
     <h2>All Posts</h2>
+    <form action="/posts" method="get">
+        <input
+            type="text" name="search" placeholder="Search"
+            value="<?= htmlspecialchars($search) ?>">
+        <button type="submit">Search</button>
+    </form>
     <?php foreach ($posts as $post): ?>
         <article>
             <h4>

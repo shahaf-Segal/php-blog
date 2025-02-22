f4b74a7c5ccc2d0581f28afb0dd0e97d7140b935
shahaf-Segal <shahaf100@outlook.co.il>
2025-02-22 21:27:04 +0200

    added search

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Core\Model;
 use Core\App;
 
+const DEFAULT_LIMIT = 10;
+
 class Post extends Model
 {
     protected static $table = 'posts';
@@ -16,10 +18,25 @@ class Post extends Model
     public $views;
     public $created_at;
 
-    public static function getRecent(int $limit): array
+    public static function getRecent(?int $limit = DEFAULT_LIMIT): array
     {
         $db = App::get('database');
         return $db->fetchAll("SELECT * FROM " . static::$table . " ORDER BY created_at DESC LIMIT ?", [$limit], static::class);
+    }
+    public static function search(?int $limit = DEFAULT_LIMIT, ?string $search = null): array
+    {
+        $db = App::get('database');
+        $query = "SELECT * FROM " . static::$table;
+        $params = [];
+        if ($search) {
+            $query .= " WHERE title LIKE ? OR content LIKE ?";
+            $params = ["%$search%", "%$search%"];
+        }
+
+        $query .= " ORDER BY created_at DESC LIMIT ?";
+        $params[] = $limit;
+
+        return $db->fetchAll($query, $params, static::class);
     }
     public function incrementViews(): void
     {

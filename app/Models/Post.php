699ce1e8700fcbf1057aa0e60b8467377adb424f
shahaf-Segal <shahaf100@outlook.co.il>
2025-02-23 22:54:07 +0200

    page to getRecent

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -18,7 +18,7 @@ class Post extends Model
     public $views;
     public $created_at;
 
-    public static function getRecent(?int $limit = DEFAULT_LIMIT, ?string $search = null): array
+    public static function getRecent(?int $limit = DEFAULT_LIMIT, ?string $search = null, ?int $page = null): array
     {
         $db = App::get('database');
         $query = "SELECT * FROM " . static::$table;
@@ -31,11 +31,31 @@ class Post extends Model
         $query .= " ORDER BY created_at DESC LIMIT ?";
         $params[] = $limit;
 
+        if ($page) {
+            $offset = ($page - 1) * $limit;
+            $query .= " OFFSET ?";
+            $params[] = $offset;
+        }
+
         return $db->fetchAll($query, $params, static::class);
     }
     public function incrementViews(): void
     {
         $db = App::get('database');
         $db->query("UPDATE " . static::$table . " SET views = views + 1 WHERE id = ?", [$this->id]);
+    }
+    public static function count(?string $search = null): int
+    {
+        $db = App::get('database');
+
+        $query = "SELECT COUNT(*) FROM " . static::$table;
+        $params = [];
+
+        if ($search) {
+            $query .= " WHERE title LIKE ? OR content LIKE ?";
+            $params = ["%$search%", "%$search%"];
+        }
+
+        return (int) $db->query($query, $params)->fetchColumn();
     }
 }

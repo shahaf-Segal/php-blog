98b02f16f634ccdf172a65558b192c4c683cfe01
shahaf-Segal <shahaf100@outlook.co.il>
2025-02-28 16:42:01 +0200

    add auth routes

--- routes.php
+++ routes.php
@@ -8,3 +8,5 @@ declare(strict_types=1);
 $router->add('/', 'GET', 'HomeController@index');
 $router->add('/posts', 'GET', 'PostController@index');
 $router->add('/posts/{id}', 'GET', 'PostController@show');
+$router->add('/login', 'GET', 'AuthController@create');
+$router->add('/login', 'POST', 'AuthController@store');

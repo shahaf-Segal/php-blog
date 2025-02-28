4e28a8026406677888df4483a9b855631bf1cd5c
shahaf-Segal <shahaf100@outlook.co.il>
2025-02-28 17:52:33 +0200

    added properties, password verify and find by email

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,5 +7,23 @@ use Core\Model;
 
 class User extends Model
 {
+
     protected static $table = 'users';
+
+    public $id;
+    public $name;
+    public $email;
+    protected $password;
+    protected $salt;
+    public $role;
+    public $created_at;
+
+    public static function findByEmail(string $email): User|false
+    {
+        return static::find(['email' => $email]);
+    }
+    public function verifyPassword(string $password): bool
+    {
+        return password_verify($password . $this->salt, $this->password);
+    }
 }

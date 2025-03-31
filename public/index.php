2433600adb3d0016975b1e05bec86823fd4c65cc
shahaf-Segal <shahaf100@outlook.co.il>
2025-03-31 17:33:35 +0300

    add user global and check in page

--- public/index.php
+++ public/index.php
@@ -7,10 +7,13 @@ require_once __DIR__ . '/../bootstrap.php';
 session_start();
 
 use Core\Router;
+use Core\View;
 
 $router = new Router();
 require_once __DIR__ . '/../routes.php';
 require_once __DIR__ . '/../helpers.php';
+
+View::globalShare('user', 'guest');
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $method = $_SERVER['REQUEST_METHOD'];

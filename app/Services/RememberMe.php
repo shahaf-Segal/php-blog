7e723672c4808ee6c92c1e75c0941a8c21852bd9
shahaf-Segal <shahaf100@outlook.co.il>
2025-05-08 21:54:35 +0300

    clear  and validate for token

--- app/Services/RememberMe.php
+++ app/Services/RememberMe.php
@@ -1,27 +1,35 @@
 <?php
+
 namespace App\Services;
+
 use App\Models\RememberToken;
 use App\Models\User;
 
-class RememberMe{
+class RememberMe
+{
     private const COOKIE_NAME = 'remember_token';
-    public static function createToken(int $user_id): RememberToken{
-        $token=RememberToken::createForUser($user_id);
+    public static function createToken(int $user_id): RememberToken
+    {
+        $token = RememberToken::createForUser($user_id);
         static::setCookie($token->token);
-        return $token;       
+        return $token;
     }
-    private static function setCookie(string $token): void{
+    private static function setCookie(string $token): void
+    {
         $expiresAt = time() +  RememberToken::TOKEN_LIFETIME;
-        setcookie(static::COOKIE_NAME, $token,
-         $expiresAt, '/', '', true, true);
-
+        setcookie(
+            static::COOKIE_NAME,
+            $token,
+            $expiresAt,
+            '/',
+            '',
+            true,
+            true
+        );
     }
-    public static function validateToken(): ?User{
-        $tokenString = $_COOKIE[static::COOKIE_NAME] ?? null;
-        if (!$tokenString) {
-            return null;
-        }
-        $token = RememberToken::findValid($tokenString);
+    public static function validateToken(): ?User
+    {
+        $token = static::getToken();
         if (!$token) {
             return null;
         }
@@ -32,12 +40,31 @@ class RememberMe{
         static::rotateCookie($token);
         return $user;
     }
-    private static function rotateCookie(RememberToken $token): void{
+    public static function clearToken(): void
+    {
+        $token = static::getToken();
+        if ($token) {
+            $token->delete();
+        }
+        static::destroyCookie();
+    }
+    private static function getToken(): ?RememberToken
+    {
+        $tokenString = $_COOKIE[static::COOKIE_NAME] ?? null;
+        if (!$tokenString) {
+            return null;
+        }
+        $token = RememberToken::findValid($tokenString);
+        return $token;
+    }
+
+    private static function rotateCookie(RememberToken $token): void
+    {
         $token->rotate();
         static::setCookie($token->token);
-        
     }
-    public static function destroyCookie(): void{
+    public static function destroyCookie(): void
+    {
         setCookie(static::COOKIE_NAME, '', time() - 3600, '/', '', true, true);
     }
 }

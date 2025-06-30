e58b622f22375165ecf7bf1a70538cf668c91c42
shahaf-Segal <shahaf100@outlook.co.il>
2025-06-30 19:35:36 +0300

    remember me checkbox on login

--- app/Views/auth/create.php
+++ app/Views/auth/create.php
@@ -1,8 +1,19 @@
 <h1>Login</h1>
 <form action="/login" method="post">
     <!-- Place for CSRF -->
-    <input type="email" name="email" placeholder="Email" required>
-    <input type="password" name="password" placeholder="Password" required>
+    <div>
+        <label for="email">Email</label>
+        <input type="email" name="email" placeholder="Email" required>
+    </div>
+    <div>
+        <label for="password">Password</label>
+        <input type="password" name="password" placeholder="Password" required>
+    </div>
+    <div>
+        <label for="remember">Remember me
+            <input type="checkbox" name="remember" id="remember">
+        </label>
+    </div>
     <!-- remember me -->
     <button type="submit">Login</button>
 </form>
